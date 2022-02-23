4e0ef2afe0613eab302208fa51eff6aa667f136e
dannyzimm <daniel.prz.rodriguez@gmail.com>
2022-02-23 09:42:41 +0100

    feat: #36 added feature and attribute compi parameters for feature-counts.

--- resources/scripts/feature-counts.sh
+++ resources/scripts/feature-counts.sh
@@ -17,7 +17,7 @@ docker run --rm \
 	-v ${workingDir}:${workingDir} \
 	-v ${gffFile}:${gffFile} \
 	pegi3s/feature-counts:${featureCountsVersion} \
-	sh -c "featureCounts -F GTF -t miRNA -g 'Name' -a ${gffFile} -o "${path_output}/all-counts.txt" "${bam_input}"*".bam""
+	sh -c "featureCounts -F GTF -t $gffFeature -g $gffAttribute -a ${gffFile} -o "${path_output}/all-counts.txt" "${bam_input}"*".bam""
 
 #sh -c "featureCounts -F GFF -t miRNA -g 'product' -a ${gffFile} -o "${path_output}/all-counts.txt" "${bam_input}"*".bam"" #for NCBI gff
 #sh -c "featureCounts -F GTF -t miRNA -g 'Name' -a ${gffFile} -o "${path_output}/all-counts.txt" "${bam_input}"*".bam"" #for miRBase gff3

6a69850650cd982f00fc1d85f318ec6f2505c5f6
dannyzimm <daniel.prz.rodriguez@gmail.com>
2022-11-25 12:51:12 +0100

    feat: implemented "run_hierarchical-clustering.R" in "hclust.sh".

--- resources/scripts/hclust.sh
+++ resources/scripts/hclust.sh
@@ -9,7 +9,7 @@ cp ${scriptsDir}/${hclustMakeTableRscript} ${workingDir}/compi_scripts/${hclustM
 contrast_label=$(echo "${comparison}" | cut -d'"' -f2 )
 
 # function to perform the hierarchical clustering analysis
-function run_hclust {
+function run_hclust_make-table {
 # $1 : ${dea_results}
 # $2 : ${path_counts}
 # $3 : ${path_conditions}
@@ -24,6 +24,19 @@ echo "[PIPELINE -- hclust]: Building the hierarchical clustering table for ${4} 
 		Rscript "${workingDir}/compi_scripts/${hclustMakeTableRscript}" "${1}" "${2}" "${3}" "${4}" "${5}" "${6}"
 }
 
+function run_hclust {
+# $1 : ${path_hclust_file}
+# $2 : ${input_contrast}
+# $3 : ${path_output}
+# $4 : ${software}
+
+echo "[PIPELINE -- hclust]: Running the hierarchical clustering analysis for ${4} results..."
+  docker run --rm \
+	-v ${workingDir}:${workingDir} \
+	pegi3s/r_data-analysis:${rdatanalysisVersion} \
+		Rscript "${workingDir}/compi_scripts/${hclustRscript}" "${1}" "${2}" "${3}" "${4}"
+}
+
 path_conditions="${conditions}"
 input_contrast="${comparison}"
 
@@ -34,7 +47,11 @@ then
 	dea_results="${path_output_docker}/$(echo EdgeR_${contrast_label} | xargs).tsv"
 	path_counts="${path_output_docker}/pipel/$(echo all-counts_edger_${contrast_label} | xargs).txt"
 	software="EdgeR"
-	run_hclust "${dea_results}" "${path_counts}" "${path_conditions}" "${input_contrast}" "${path_output_docker}" "${software}"
+	path_hclust_file="${path_output_docker}/$(echo hclust_EdgeR_${contrast_label} | xargs).tsv"
+	# prepare tables
+	run_hclust_make-table "${dea_results}" "${path_counts}" "${path_conditions}" "${input_contrast}" "${path_output_docker}" "${software}"
+	# hierarchical clustering
+	run_hclust "${path_hclust_file}" "${input_contrast}" "${path_output_docker}" "${software}"
 fi
 
 if [[ ${selectDEAsoftware} == 'deseq' ]] || [[ ${selectDEAsoftware} == 'both' ]]
@@ -43,7 +60,11 @@ then
 	dea_results="${path_output_docker}/$(echo DESeq2_${contrast_label} | xargs).tsv"
 	path_counts="${path_output_docker}/pipel/$(echo all-counts_deseq_${contrast_label} | xargs).txt"
 	software="DESeq2"
-	run_hclust "${dea_results}" "${path_counts}" "${path_conditions}" "${input_contrast}" "${path_output_docker}" "${software}"
+	path_hclust_file="${path_output_docker}/$(echo hclust_DESeq2_${contrast_label} | xargs).tsv"
+	# prepare tables
+	run_hclust_make-table "${dea_results}" "${path_counts}" "${path_conditions}" "${input_contrast}" "${path_output_docker}" "${software}"
+	# hierarchical clustering
+	run_hclust "${path_hclust_file}" "${input_contrast}" "${path_output_docker}" "${software}"
 fi
 
 if [[ ${selectDEAsoftware} == 'both' ]]
@@ -53,5 +74,9 @@ then
 	# uses the counts of EdgeR DESeq2 and
 	path_counts="${workingDir}/${outDir}/${edgOut}/pipel/$(echo all-counts_edger_${contrast_label} | xargs).txt"
 	software="DESeq2-EdgeR"
-	run_hclust "${dea_results}" "${path_counts}" "${path_conditions}" "${input_contrast}" "${path_output_docker}" "${software}"
+	path_hclust_file="${path_output_docker}/$(echo hclust_DESeq2-EdgeR_${contrast_label} | xargs).tsv"
+	# prepare tables
+	run_hclust_make-table "${dea_results}" "${path_counts}" "${path_conditions}" "${input_contrast}" "${path_output_docker}" "${software}"
+	# hierarchical clustering
+	run_hclust "${path_hclust_file}" "${input_contrast}" "${path_output_docker}" "${software}"
 fi

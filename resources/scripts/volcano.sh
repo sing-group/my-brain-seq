b05247d43adbb59ec2cae9596d9d8038f996cf4b
dannyzimm <daniel.prz.rodriguez@gmail.com>
2023-01-11 10:09:41 +0100

    fix: Race condition on "volcano.sh" using flock.

--- resources/scripts/volcano.sh
+++ resources/scripts/volcano.sh
@@ -6,9 +6,9 @@ set -o errexit
 function cp_and_lock {
 # $1 : script to copy  # $2 : task name
 	(
-	flock -n 200 || echo "[PIPELINE -- "${2}"]: ${1} is locked, cp omitted."
+	flock 200 || echo "[PIPELINE -- "${2}"]: ${1} is locked, cp omitted."
 	#Makes a copy of the scripts used in the analysis to working-dir
-	cp ${scriptsDir}/${1} ${workingDir}/compi_scripts/${1}
+	cp -n ${scriptsDir}/${1} ${workingDir}/compi_scripts/${1}
 	) 200>/var/lock/${1}.lock
 }
 

a9a84439206689cf2aaf773806e1a4663129e98d
dannyzimm <daniel.prz.rodriguez@gmail.com>
2023-01-18 13:52:05 +0100

    feat: added the "get_output_dir" function to "functions.sh".
    This function will be used to automatically build the path to
    the DESeq2 and EdgeR results. The path points to a subdirectory with the name of each contrast.

--- resources/scripts/functions.sh
+++ resources/scripts/functions.sh
@@ -11,3 +11,14 @@ function cp_and_lock {
 	cp -n ${3}/${1} ${workingDir}${target_dir}${1}
 	) 200>/var/lock/${1}.lock
 }
+
+# get output path for the files saved in 6_deseq2, and 6_edger directories
+# to use this function assign it to a variable like this: result=$(get_output_dir)
+function get_output_dir {
+	# $1 : software used (deseq/edger)  # $2 : ${comparison}
+	local comparison_label=$(echo "$2" | cut -d'=' -f1 | tr -d \" | sed -e 's/ /\\ /g' | xargs)
+	if [[ $1 == 'deseq' ]];     then local soft_dir="${dsqOut}";
+	elif [[ $1 == 'edger' ]];   then local soft_dir="${edgOut}"; fi
+	local path_output="${workingDir}/${outDir}/${soft_dir}/${comparison_label}/"
+	echo "$path_output"
+}

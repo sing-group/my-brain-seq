98ee2c7c8fd2ad7912ce44818b59f0cd94c87dfe
dannyzimm <daniel.prz.rodriguez@gmail.com>
2023-02-09 12:51:35 +0100

    feat: added variable with myBrain-seq version in "make_run-sh.sh". Solved bug in previous commit.

--- resources/init-working-dir/make_run-sh.sh
+++ resources/init-working-dir/make_run-sh.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+
 #---------------------------------------------------------------------------------------
 # DESCRIPTION:
-#	This script builds the runner of the miRNA_pipeline using the compi.parameters file
-#	as reference. The runner will mount all the needed Docker volumes to run the
-#	miRNA-Seq analysis and build a directory for the logs.
-#	The resulting file will be saved in the same directoy as the compi.parameters file,
-#	and it will be named as "run_<name of the workingDir>.sh".
+#	This script builds the runner of the miRNA_pipeline using the compi.parame-
+#   ters file as reference. The runner will mount all the needed Docker volumes
+#   to run the miRNA-Seq analysis and build a directory for the logs.
+#	The resulting file will be saved in the same directoy as the compi.parame-
+#   ters file, and it will be named as "run_<name of the workingDir>.sh".
 #
 # PARAMETERS:
 #	$1: Absolute path to the compi.parameters file.
@@ -14,8 +15,8 @@
 #	./make_run-sh.sh /path/to/compi.parameters
 #
 # WARNING:
-#	The compi.parameters file should include the following parameters (the order is not
-#	relevant):
+#	The compi.parameters file should include the following parameters
+#   (the order is not relevant):
 #		workingDir=/...
 #		fastqDir=/...
 #		adapter=/...
@@ -24,11 +25,13 @@
 #		conditions=/...
 #		contrast=/...
 #	
-#	A template of this file could be created using the make_compi-parameters.sh script.
+#	A template of this file could be created using "make_compi-parameters.sh".
 
 #---------------------------------------------------------------------------------------
 
-# function to perform the enrichment analysis
+# PARAMETERS
+myBrain_seq_version='latest'
+
 # to use: parameter_value=$(get_compi_parameter X Y)
 function get_compi_parameter {
 # $1 : ${1}   # $2 : compi parameter name  
@@ -55,16 +58,18 @@ printf "mkdir -p \${workingDir}/logs/\${timestamp}\n" >> "${workingDir}/.run_${s
 
 # write create the run.sh file
 if [[ ! -z "${bwtIndex}" ]]; then
-# if bowtie index
-bowtie_dir="$(dirname ${bwtIndex})"
-printf "docker run -it --rm @\n\t\t-v /var/run/docker.sock:/var/run/docker.sock @\n\t\t-v ${workingDir}:${workingDir} @\n\t\t-v ${fastqDir}:${fastqDir} @\n\t\t-v ${bowtie_dir}:${bowtie_dir} @\n\t\t-v ${gffFile}:${gffFile} @\n\t\t-v ${conditions}:${conditions} @\n\t\t-v ${contrast}:${contrast} @\n\t\tsinggroup/my-brain-seq @\n\t\t\t--logs ${workingDir}/logs/\${timestamp}/tasks @\n\t\t\t-pa ${1} @\n\t\t\t-o @\n\t\t\t--num-tasks 5 @\n\t\t\t-- --dea both @\n\t\t2>&1 | tee ${workingDir}/logs/\${timestamp}/compi.log" >> "${workingDir}/.run_${studyName}.sh"
+    # if bowtie index
+    bowtie_dir="$(dirname ${bwtIndex})"
+    printf "docker run -it --rm @\n\t\t-v /var/run/docker.sock:/var/run/docker.sock @\n\t\t-v ${workingDir}:${workingDir} @\n\t\t-v ${fastqDir}:${fastqDir} @\n\t\t-v ${bowtie_dir}:${bowtie_dir} @\n\t\t-v ${gffFile}:${gffFile} @\n\t\t-v ${conditions}:${conditions} @\n\t\t-v ${contrast}:${contrast} @\n\t\tsinggroup/my-brain-seq:${myBrain_seq_version} @\n\t\t\t--logs ${workingDir}/logs/\${timestamp}/tasks @\n\t\t\t-pa ${1} @\n\t\t\t-o @\n\t\t\t--num-tasks 5 @\n\t\t\t-- --dea both @\n\t\t2>&1 | tee ${workingDir}/logs/\${timestamp}/compi.log" >> "${workingDir}/.run_${studyName}.sh"
+
 elif [[ ! -z "${genome}" ]]; then
-# if genome
-printf "docker run -it --rm @\n\t\t-v /var/run/docker.sock:/var/run/docker.sock @\n\t\t-v ${workingDir}:${workingDir} @\n\t\t-v ${fastqDir}:${fastqDir} @\n\t\t-v ${genome}:${genome} @\n\t\t-v ${gffFile}:${gffFile} @\n\t\t-v ${conditions}:${conditions} @\n\t\t-v ${contrast}:${contrast} @\n\t\tsinggroup/my-brain-seq @\n\t\t\t--logs ${workingDir}/logs/\${timestamp}/tasks @\n\t\t\t-pa ${1} @\n\t\t\t-o @\n\t\t\t--num-tasks 5 @\n\t\t\t-- --dea both @\n\t\t2>&1 | tee ${workingDir}/logs/\${timestamp}/compi.log" >> "${workingDir}/.run_${studyName}.sh"
+    # if genome
+    printf "docker run -it --rm @\n\t\t-v /var/run/docker.sock:/var/run/docker.sock @\n\t\t-v ${workingDir}:${workingDir} @\n\t\t-v ${fastqDir}:${fastqDir} @\n\t\t-v ${genome}:${genome} @\n\t\t-v ${gffFile}:${gffFile} @\n\t\t-v ${conditions}:${conditions} @\n\t\t-v ${contrast}:${contrast} @\n\t\tsinggroup/my-brain-seq:${myBrain_seq_version} @\n\t\t\t--logs ${workingDir}/logs/\${timestamp}/tasks @\n\t\t\t-pa ${1} @\n\t\t\t-o @\n\t\t\t--num-tasks 5 @\n\t\t\t-- --dea both @\n\t\t2>&1 | tee ${workingDir}/logs/\${timestamp}/compi.log" >> "${workingDir}/.run_${studyName}.sh"
+
 fi
 
 # put backslashes instead of @
-cat ${workingDir}/.run_${studyName}.sh | tr '@' '\' > ${workingDir}/run_${studyName}.sh &> /dev/null
+cat ${workingDir}/.run_${studyName}.sh | tr '@' '\' &> /dev/null > ${workingDir}/run_${studyName}.sh
 rm -f ${workingDir}/.run_${studyName}.sh
 
 # give run permisions

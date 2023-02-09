10db23082457ede9f12654272832493fa8abf812
dannyzimm <daniel.prz.rodriguez@gmail.com>
2023-02-09 11:56:36 +0100

    feat: "make_run-sh.sh" does not need an entrypoint.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,21 +2,19 @@
 
 if [[ $# -gt 0 && "$1" == "--help" || "$1" == "-h" ]]; then
 	/compi run -p /pipeline*.xml "$@"
-	echo -e "\nAdditional CGA commands:"
 	echo -e "\t- init_working_dir.sh /working_dir: initializes the working directory with a base parameters file."
-	echo -e "\t- process_logs.sh /working_dir: processes the Compi logs directory to create a report of tasks with errors or warnings."
+	echo -e "\t- make_run-sh.sh /path/to/compi.parameters: build the runner for myBrain-Seq analysis."
+
+elif [ $# -gt 0 ] && [ "$1" == "resume" ]; then
+	/compi resume -p /pipeline*.xml "${@:2}"
+
+elif [ $# -gt 0 ] && [ "$1" == "init_working_dir.sh" ]; then
+	/scripts/init_working_dir.sh "${@:2}"
+
+elif [ $# -gt 0 ] && [ "$1" == "make_run-sh.sh" ]; then
+	/init-working-dir/make_run-sh.sh "${@:2}"
+
 else
-	if [ $# -gt 0 ] && [ "$1" == "resume" ]; then
-		/compi resume -p /pipeline*.xml "${@:2}"
-	else
-		if [ $# -gt 0 ] && [ "$1" == "init_working_dir.sh" ]; then
-			/scripts/init_working_dir.sh "${@:2}"
-		else
-			if [ $# -gt 0 ] && [ "$1" == "process_logs.sh" ]; then
-				process_logs.sh "${@:2}"
-			else
-				/compi run -p /pipeline*.xml "$@"
-			fi
-		fi
-	fi
+	/compi run -p /pipeline*.xml "$@"
+
 fi

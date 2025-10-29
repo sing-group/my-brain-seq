17fc7499794cf8389ed31adcdfa66168f19b483f
Hugo López Fernández <hlfernandez@uvigo.es>
2025-10-29 17:47:13 +0100

    Fixes sam-to-bam task
    This version fixes a bug in this task that was not running the
    samtools index command properly and .bam.bai files were not created.

--- resources/init-working-dir/run.sh
+++ resources/init-working-dir/run.sh
@@ -10,7 +10,7 @@ function get_compi_parameter {
     cat "${1}" | grep "${2}=" | cut -d'=' -f2
 }
 
-MYBRAIN_SEQ_VERSION=${MYBRAIN_SEQ_VERSION-1.2.2}
+MYBRAIN_SEQ_VERSION=${MYBRAIN_SEQ_VERSION-1.2.3}
 
 FULL_COMPI_PARAMS_FILE=$1
 ADDITIONAL_COMPI_PARAMS="${2:--num-tasks 5}"
--- resources/scripts/sam-to-bam.sh
+++ resources/scripts/sam-to-bam.sh
@@ -10,18 +10,14 @@ bam_output="$(echo $bam_output | tr -s '/')" # convert the path to single slash
 filebam=$(echo ${file} | sed 's/\.sam//')
 filebam=$(echo $filebam | tr -s '/')  # convert the path to single slash
 
-#test if the file exist, if so removes it
 if [ -f "${bam_output}/${filebam}.bam" ]; then
 	echo "[MBS | sam-to-bam]: ${filebam}.bam already exists, removing ${filebam}.bam and ${filebam}.bam.bai."
-	rm -f "${bam_output}/${filebam}.bam*"
+	rm -f "${bam_output}/${filebam}.bam"
+	rm -f "${bam_output}/${filebam}.bam.bai"
 fi
-
-##replaces $working_dir with ${workingDir}/
-#bam_output=$(echo $bam_output | cut -d / -f 3- | awk '{print "${workingDir}/" $0}')
 
 echo "[MBS | sam-to-bam]: Converting ${filebam}.sam to bam."
 docker run --rm \
 	-v ${workingDir}:${workingDir} \
 	pegi3s/samtools_bcftools:${samtoolsVersion} \
-	sh -c "samtools sort "${bam_output}${file}" > "${bam_output}${filebam}.bam"" \
-	sh -c "samtools index "${bam_output}${filebam}.bam""
+		sh -c "samtools sort \"${bam_output}${file}\" > \"${bam_output}${filebam}.bam\" && samtools index \"${bam_output}${filebam}.bam\""
